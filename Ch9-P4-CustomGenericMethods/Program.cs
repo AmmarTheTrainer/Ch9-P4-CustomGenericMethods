716888903b59b244cca99fb2128ead81d5287186
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-09-26 18:02:42 +0500

    first commit from office
    head office

---
+++ Ch9-P4-CustomGenericMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch9_P4_CustomGenericMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
